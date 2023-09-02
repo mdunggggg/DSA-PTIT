422bdd65c39b15ba3209019dd227f83772b409da
ManhhDungg <97603997+mdunggggg@users.noreply.github.com>
2023-09-02 22:07:06 +0700

    Update DSA02021.cpp

--- DSA02021.cpp
+++ DSA02021.cpp
@@ -1,47 +1,47 @@
-/*  Hoang Manh Dung - D21 ProPTIT  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define Faster() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-const double PI = 2 * acos(0);
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define endl '\n'
+
 const int MOD = 1e9 + 7;
 const int MAX = 1e6 + 5;
-void Process(){
+
+void process()
+{
     string s;
-    long long k;
+    long long k, n;
     cin >> s >> k;
-    long long n = s.size();
-    vector<long long>a;
-    a.push_back(n);
-    int i = 0;
-    while(true){
-        if(2 * a[i] <= 2e18){
-            a.push_back(2 * a[i]);
-            i++;
+    n = s.size();
+    while (n < k)
+        n *= 2;
+    while (n > 1ll * s.size())
+    {
+        if (k > n / 2)
+        {
+            k -= n / 2;
+            if (k >= 2)
+                k--;
+            else
+                k = n / 2;
         }
-        else    
-            break;
+        n /= 2;
     }
-    int pos = a.size();
-    s = " " + s;
-    --pos;
-    while(k > n){
-        if(a[pos] < k){
-            long long idx = k - a[pos];
-            if(idx != 1){
-                k = idx - 1;
-            }
-            else{
-                k = a[pos - 1] - 1;
-            }
-        }
-        --pos;
-    }
-    cout << s[k] << '\n';
+    cout << s[k - 1] << endl;
 }
-int main(){
-    Faster();
-    int t; cin >> t;
-    while(t--)
-        Process();
+int main()
+{
+    faster
+        test
+        process();
 }
-    
